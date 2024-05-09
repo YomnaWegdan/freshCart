d2235ce1540c031dae59222234685460cb7bc066
YomnaWegdan <101523679+YomnaWegdan@users.noreply.github.com>
2024-05-09 23:39:30 +0300

    Update NotFound.jsx

--- src/Components/NotFound/NotFound.jsx
+++ src/Components/NotFound/NotFound.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
+import error from '../../Assets/images/error.svg'
 
 export default function NotFound() {
   return (
-    <div>
-      
+    <div className=' vh-100'>
+      <div className=' d-flex justify-content-center align-items-center h-100'>
+        <img src={error} alt='error-img' />  
+      </div>
     </div>
   )
 }
