8eaecf379f30523fe92ab1b77a16f5e6f45945d0
YomnaWegdan <101523679+YomnaWegdan@users.noreply.github.com>
2024-05-24 23:08:17 +0300

    Update NotFound.jsx

--- src/Components/NotFound/NotFound.jsx
+++ src/Components/NotFound/NotFound.jsx
@@ -5,7 +5,7 @@ export default function NotFound() {
   return (
     <div className=' vh-100'>
       <div className=' d-flex justify-content-center align-items-center h-100'>
-        <img src={error} alt='error-img' />  
+        <img src={error} alt='error-img' className='w-100' />  
       </div>
     </div>
   )
