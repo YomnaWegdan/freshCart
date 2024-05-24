93970b49479b84cb982e595d6d0b7b91b2b25b7e
YomnaWegdan <101523679+YomnaWegdan@users.noreply.github.com>
2024-05-24 23:05:50 +0300

    Update BrandDetails.jsx

--- src/Components/BrandDetails/BrandDetails.jsx
+++ src/Components/BrandDetails/BrandDetails.jsx
@@ -13,11 +13,11 @@ export default function BrandDetails() {
 
   return (
     <Fragment>
-    <div className="row justify-content-center align-items-center align-items-center  vh-100 ">
-    <div className='col-md-6 h-75 bg-light d-flex align-items-center justify-content-center rounded-3 shadow shadow'>
-       <img src={data.image} alt={data.title} key={data._id} className="w-50"/>
+    <div className="row justify-content-center align-items-center align-items-center  min-vh-100 ">
+    <div className='col-md-6 p-5 bg-light d-flex align-items-center justify-content-center rounded-3 shadow shadow'>
+       <img src={data.image} alt={data.title} key={data._id} className="w-75"/>
     </div> 
-    <div className='details-card col-md-6 h-50 bg-light d-flex align-items-center justify-content-center rounded-3 shadow '>
+    <div className='details-card col-md-6 p-5 bg-light d-flex align-items-center justify-content-center rounded-3 shadow '>
     <h3 className="h5 text-main ">{data.name}</h3>
     </div>
     </div>
